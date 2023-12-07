97d18bc8427a9d4e8cc66025683025ddfa2a3f6d
LudusDev <123208783+LudusDev@users.noreply.github.com>
2023-12-07 20:22:47 +0700

    Delete assets/js directory
    error

--- assets/js/typing-animation.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing", {
-  strings: ["Digital Marketing", "Programmatic advertising",  "Creatives", "Branding", "Social Media"],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true,
-});
