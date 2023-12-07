07cd6da848316020f6e46665d7cc9d558deea1cf
LudusDev <123208783+LudusDev@users.noreply.github.com>
2023-12-07 22:35:37 +0700

    Add files via upload

---
+++ assets/custom/js/typing-animation.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+  strings: ["Digital Marketing", "Programmatic advertising",  "Creatives", "Branding", "Social Media"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
